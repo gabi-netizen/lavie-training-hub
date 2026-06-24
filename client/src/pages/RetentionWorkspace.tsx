c19d329363f65307ff096d594c76aaa8472d5725
gabi-netizen <2.77184442e+08+gabi-netizen@users.noreply.github.com>
2026-06-24 16:24:15 +0000

    Revert: restore original queue logic - show all leads in Incoming

--- client/src/pages/RetentionWorkspace.tsx
+++ client/src/pages/RetentionWorkspace.tsx
@@ -482,7 +482,7 @@ export default function RetentionWorkspace({ agentName: agentNameProp }: { agent
 
   // Tab filtering - show ALL leads in queue
   const queueLeads = useMemo(
-    () => filteredLeads.filter((l) => l.workStatus !== "callback" && l.workStatus !== "follow_up"),
+    () => filteredLeads,
     [filteredLeads]
   );
 
